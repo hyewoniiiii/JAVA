75fc5bda17b576e1342224af9441af7a1d653582
hyewoniiiii <97446430+hyewoniiiii@users.noreply.github.com>
2022-01-12 00:15:49 +0900

    Create Day1_Test.java

---
+++ Day1_Test.java
@@ -0,0 +1,38 @@
+public class Test {
+	public static void main(String[] args) {
+		
+		System.out.println("#1");
+		System.out.println("월급: 150만 원");
+		System.out.println("세금을 제외한 연봉: " + (150 * 12 * 9/10) + "만 원");
+		System.out.println();
+		
+		System.out.println("#2");
+		int total = 75;
+		System.out.println("시험 점수: 30, 40, 5");
+		System.out.println("총점: " + total);
+		System.out.println("평균: " + (total / 3));
+		System.out.println();
+		
+		System.out.println("#3");
+		System.out.println("가로: 3, 세로: 6");
+		System.out.println("삼각형의 넓이 = " + (3 * 6 * 1/2));
+		System.out.println();
+		
+		System.out.println("#4");
+		System.out.println("내가 가진 돈: 만 원");
+		System.out.println("-> 신라면 1800원, 콜라 1300원 구입");
+		System.out.println("거스름돈 = 6900원");
+		System.out.println("그 때 500원 개수: " + (6900 / 500));
+		System.out.println("그 때 100원 개수: " + ((6900 % 500) / 100));
+		System.out.println();
+		
+		System.out.println("#2557");
+		System.out.println("Hello World!");
+		System.out.println();
+		
+		System.out.println(("#10718"));
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+		
+	}
+}
