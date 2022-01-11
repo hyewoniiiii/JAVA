dae4b56a11b27aa978da63a751f33c1b22c71d2f
hyewoniiiii <97446430+hyewoniiiii@users.noreply.github.com>
2022-01-12 00:15:28 +0900

    Create Day1_VariableEx01.java

---
+++ Day1_VariableEx01.java
@@ -0,0 +1,40 @@
+public class VariableEx01 {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		System.out.println(2 + 7);
+		System.out.println(2 + 10);
+		System.out.println("안녕하세요");
+		
+		// 변수: 저장 (값을 기억)
+		// 이름을 저장하는 변수 이름: name(String)
+		//                       본인 이름 저장하기
+		
+		String name = "장혜원";
+		
+		// 나이를 저장하는 변수 이름: age(int)
+		//                       22
+		int age;
+		age = 22;
+		
+		// 주소를 저장하는 변수 이름: address(String)
+		//                       용인시 수지구
+		String address = "용인시 수지구";
+				
+		// 혈액형 저장하는 변수 이름: blood(char)
+		//                       B
+		char blood = 'B';
+		
+		// 키를 저장하는 변수 이름: height(double)
+		//                     164
+		double height = 164.0;
+		
+		// 자동완성 -> 명령어 + ctrl + space
+		// sysout -> System.ouot.println();
+		System.out.println();
+		
+		// 정수 3개를 저장하는 공간(변수)
+		int a = 10, b = 20, c = 30;
+		
+	}
+}
