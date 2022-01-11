e3a911931ad236870e02edfc20a3aa400b9ef014
hyewoniiiii <97446430+hyewoniiiii@users.noreply.github.com>
2022-01-12 00:14:55 +0900

    Create Day1_PrintlnAdd.java

---
+++ Day1_PrintlnAdd.java
@@ -0,0 +1,22 @@
+public class PrintlnAdd {
+	public static void main(String[] args) {
+		
+		// + 기능
+		// 숫자 + 숫자 = 연산 (계산 가능)
+		// 문자 + 문자 = 연결
+		
+		System.out.println("안녕" + "하세요");
+		System.out.println("이름: " + "장혜원");
+		System.out.println("나이: " + "22");
+		
+		// 10과 10을 더한 값은: 20
+		// 세미콜론 ; -> (마침표) 문장의 끝!
+		
+		// 연결하는 순서 왼쪽부터
+		System.out.println("10과 10을 더한 값: " + (10 + 10));
+		System.out.println(10 + "과 " + 10 + "을 더한 값: " + (10 + 10));
+		
+		
+		
+	}
+}
