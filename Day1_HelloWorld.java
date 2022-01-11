b1234bbb7e8a1f8a16aaacfcd5e9133caf1373af
hyewoniiiii <97446430+hyewoniiiii@users.noreply.github.com>
2022-01-12 00:14:21 +0900

    Create Day1_HelloWorld.java

---
+++ Day1_HelloWorld.java
@@ -0,0 +1,48 @@
+public class Day1_HelloWorld {
+	public static void main(String[] args) {
+		// 검은색, 자주색, 노란색 -> 번역 대상 됨
+		// 컴파일: 내가 작성한 코드를 기계어(0, 1)로 번역
+		
+		// main -> 프로그램을 시작하면 가장 먼저 실행하는 함수
+		
+		// jvm -> 가상머신 ex) 스마트폰
+		// jdk -> 가상머신 + 개발도구 (명령어들, 디버깅, 라이브러리)
+		// jre -> 가상머신 + 개발도구 (읽기모드)
+		
+		// println() -> 괄호 안에 결과를 집어 넣으면 콘솔에 보여진다.(출력)
+		
+		// 숫자
+		// 소수점이 없는 숫자: 정수(int)
+		System.out.println(10);
+		
+		// 소수점이 있는 숫자: 실수(float)
+		System.out.println(1.111);
+		
+		// 문자
+		// 한 문자
+		System.out.println('A');
+		
+		// 문자 여러 개
+		System.out.println("안녕하세요!");
+		
+		System.out.println(1 + 1);
+		System.out.println(1 - 1);
+		System.out.println(1 * 1);
+		
+		// 나눗셈 몫: /, 나머지 값: %
+		System.out.println(1 / 1);
+		System.out.println(1 % 1);
+		
+		// 실습 println
+		// 이름: 장혜원
+		// 나이: 22
+		// 사는 곳: 경기도 용인시
+		
+		System.out.println("이름: 장혜원");
+		System.out.println("나이: 22");
+		System.out.println("사는 곳: 경기도 용인시");
+		
+		
+		
+	}
+}
