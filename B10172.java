5138f7a03f9d249d51e42f5d087168f09e5ab23c
hyewoniiiii <97446430+hyewoniiiii@users.noreply.github.com>
2022-01-12 23:32:11 +0900

    Create B10172.java

---
+++ B10172.java
@@ -0,0 +1,11 @@
+public class B10172 {
+	public static void main(String[] args) {
+		
+		System.out.println("|\\_/|\r\n" + 
+				"|q p|   /}\r\n" + 
+				"( 0 )\"\"\"\\\r\n" + 
+				"|\"^\"`    |\r\n" + 
+				"||_/=\\\\__|");
+		
+	}
+}
