f9b0382071e9ab08113a227df6fcb33de6400661
hyewoniiiii <97446430+hyewoniiiii@users.noreply.github.com>
2022-01-12 23:31:06 +0900

    Create B10717.java

---
+++ B10717.java
@@ -0,0 +1,10 @@
+public class B10171 {
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\\r\n" + 
+				" )  ( ')\r\n" + 
+				"(  /  )\r\n" + 
+				" \\(__)|");
+		
+	}
+}
