65e8560b02518fbe09951d0a664d979f32ed3ad1
hyewoniiiii <97446430+hyewoniiiii@users.noreply.github.com>
2022-01-17 23:03:56 +0900

    Create Day4_for_B8393.java

---
+++ Day4_for_B8393.java
@@ -0,0 +1,20 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class B8393 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int total = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			total = total + i;
+		}
+		
+		System.out.println(total);
+	}
+
+}
